7959bf316ac7dbf582f7ee38fa5dd544b498af74
BHUMIKA SHIMPI <bhumikashimpi2005@gmail.com>
2025-06-20 15:51:40 +0530

    Create Dijkstra.py

---
+++ Dijkstra.py
@@ -0,0 +1,35 @@
+graph={
+    'A' : [('B',1),('C',4)],
+    'B' : [('A',1),('C',2),('D',1)],
+    'C' : [('A',4),('B',2),('D',1)],
+    'D' :[('B',5),('C',1)]
+}
+
+source='A'
+
+distances={node:float('inf') for node in graph }
+distances[source]=0
+visited=set()
+
+while len(visited)<len(graph):
+    min_node=None
+    min_distance=float('inf')
+
+    for node in graph:
+        if node not in visited and distances[node]<min_distance:
+             min_distance=distances[node]
+             min_node=node
+
+    if min_node is None:
+        break
+
+    visited.add(min_node)
+
+    for neighbour,weight in graph[min_node]:
+        new_distance=distances[min_node]+weight
+        if new_distance<distances[neighbour]:
+            distances[neighbour]=new_distance
+ 
+print()
+for node,dist in distances.items():
+    print(f"Shortest distance from {source} to {node} : {dist}")
